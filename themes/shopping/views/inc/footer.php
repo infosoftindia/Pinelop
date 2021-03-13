0e26f4ccfb808aa4d327b3350683d01562aef813
Prashant Rijal <prashantrijal.com.np@gmail.com>
2021-03-13 11:36:17 +0530

    Privacy Policy

--- themes/shopping/views/inc/footer.php
+++ themes/shopping/views/inc/footer.php
@@ -1,5 +1,3 @@
-		
-		
 		<!-- START FOOTER -->
 		<footer class="footer_dark">
 			<div class="footer_top">
@@ -8,7 +6,7 @@
 						<div class="col-lg-3 col-md-6 col-sm-12">
 							<div class="widget">
 								<div class="footer_logo">
-									<a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/logo_light.png" alt="logo"></a>
+									<a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/logo_light.png" alt="logo"></a>
 								</div>
 								<p>If you are going to use of Lorem Ipsum need to be sure there isn't hidden of text</p>
 							</div>
@@ -26,11 +24,11 @@
 							<div class="widget">
 								<h6 class="widget_title">Useful Links</h6>
 								<ul class="widget_links">
-									<li><a href="<?=site_url()?>">Home</a></li>
-									<li><a href="<?=site_url('about')?>">About Us</a></li>
-									<li><a href="<?=site_url('categories')?>">Categories</a></li>
-									<li><a href="<?=site_url('shop')?>">Shop</a></li>
-									<li><a href="<?=site_url('contact-us')?>">Contact</a></li>
+									<li><a href="<?= site_url() ?>">Home</a></li>
+									<li><a href="<?= site_url('about') ?>">About Us</a></li>
+									<li><a href="<?= site_url('categories') ?>">Categories</a></li>
+									<li><a href="<?= site_url('shop') ?>">Shop</a></li>
+									<li><a href="<?= site_url('contact-us') ?>">Contact</a></li>
 								</ul>
 							</div>
 						</div>
@@ -38,8 +36,9 @@
 							<div class="widget">
 								<h6 class="widget_title">Other Links</h6>
 								<ul class="widget_links">
-									<li><a href="<?=site_url('terms-and-conditions')?>">Terms & Condition</a></li>
-									<li><a href="<?=site_url('faq')?>">FAQ</a></li>
+									<li><a href="<?= site_url('terms-and-conditions') ?>">Terms & Condition</a></li>
+									<li><a href="<?= site_url('privacy-policy') ?>">Privacy Policy</a></li>
+									<li><a href="<?= site_url('faq') ?>">FAQ</a></li>
 									<li><a href="#">Help</a></li>
 								</ul>
 							</div>
@@ -62,15 +61,15 @@
 								<ul class="contact_info contact_info_light">
 									<li>
 										<i class="ti-location-pin"></i>
-										<p><?=getenv('address')?></p>
+										<p><?= getenv('address') ?></p>
 									</li>
 									<li>
 										<i class="ti-email"></i>
-										<a href="mailto:<?=getenv('email')?>"><?=getenv('email')?></a>
+										<a href="mailto:<?= getenv('email') ?>"><?= getenv('email') ?></a>
 									</li>
 									<li>
 										<i class="ti-mobile"></i>
-										<p><a href="tel:<?=getenv('mobile')?>"><?=getenv('mobile')?></a></p>
+										<p><a href="tel:<?= getenv('mobile') ?>"><?= getenv('mobile') ?></a></p>
 									</li>
 								</ul>
 							</div>
@@ -86,11 +85,11 @@
 						</div>
 						<div class="col-md-6">
 							<ul class="footer_payment text-center text-lg-right">
-								<li><a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/visa.png" alt="visa"></a></li>
-								<li><a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/discover.png" alt="discover"></a></li>
-								<li><a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/master_card.png" alt="master_card"></a></li>
-								<li><a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/paypal.png" alt="paypal"></a></li>
-								<li><a href="#"><img src="<?=site_url('themes/shopping/assets/')?>images/amarican_express.png" alt="amarican_express"></a></li>
+								<li><a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/visa.png" alt="visa"></a></li>
+								<li><a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/discover.png" alt="discover"></a></li>
+								<li><a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/master_card.png" alt="master_card"></a></li>
+								<li><a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/paypal.png" alt="paypal"></a></li>
+								<li><a href="#"><img src="<?= site_url('themes/shopping/assets/') ?>images/amarican_express.png" alt="amarican_express"></a></li>
 							</ul>
 						</div>
 					</div>
@@ -99,13 +98,10 @@
 		</footer>
 		<!-- END FOOTER -->
 
-		<a href="#" class="scrollup" style="display: none;"><i class="ion-ios-arrow-up"></i></a> 
-		
-		<?=$js?>
+		<a href="#" class="scrollup" style="display: none;"><i class="ion-ios-arrow-up"></i></a>
+
+		<?= $js ?>
 
 		</body>
+
 		</html>
-
-
-
-
